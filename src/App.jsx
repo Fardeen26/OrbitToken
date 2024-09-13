cde3ca8a1ae8f5e6567899185e56d846f4eaa135
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-13 23:29:14 +0530

    fix the home page bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import WalletContextProvider from './components/ConnectionProvider'
-import { Divider } from "@nextui-org/react"
 
 function App() {
   return (
     <div>
       <WalletContextProvider>
         <Navbar />
-        <Divider orientation='horizontal' className='h-[1px] w-full' />
+        <hr />
         <Hero />
-        <Divider orientation='horizontal' className='h-[1px] w-full' />
+        <hr />
         <Footer />
       </WalletContextProvider>
     </div>
