320e3e998c8661ebb3e30b8aaa759edb9ea5eb8c
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-09 18:23:55 +0530

    fix ui bug in dark mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className='flex items-center justify-between px-5 pb-3 transition-all bg-white text-black dark:bg-black dark:text-white'>
+        <div className='flex items-center justify-between px-5 pb-3 pt-4 transition-all bg-white text-black dark:bg-black dark:text-white'>
             <div className="text-xs pt-0">Made by <a href="https://x.com/fardeen14693425" target="_blank" className='underline'>Fardeen</a> </div>
             <div className="flex pt-0 text-xs gap-2">
                 <a href="https://x.com/fardeen14693425" target="_blank" className='underline'>Twitter</a>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,9 @@
-
-
 const Hero = () => {
     return (
-        <div className="h-screen flex flex-col items-center">
+        <div className="h-screen flex flex-col items-center dark:bg-black">
             <div className='flex flex-col items-center mt-44 p-0 transition-all bg-white dark:bg-black'>
                 <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-8xl/none dark:text-white'>OrbitToken</h1>
                 <p className='text-3xl mt-2 text-gray-700 dark:text-gray-300 tracking-tighter'>The Solana Wallet Adaptor that let you what you want to do with your SOL</p>
-
             </div>
         </div>
     )
