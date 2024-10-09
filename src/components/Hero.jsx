ee9cb87ae318e9b040fcbb769c22e18116310494
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-09 18:17:02 +0530

    fix footer height problem

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
+
+
 const Hero = () => {
     return (
-        <div className="pt-48 pb-60 flex flex-col items-center justify-center">
-            <div className='flex flex-col items-center p-0 transition-all bg-white dark:bg-black'>
+        <div className="h-screen flex flex-col items-center">
+            <div className='flex flex-col items-center mt-44 p-0 transition-all bg-white dark:bg-black'>
                 <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-8xl/none dark:text-white'>OrbitToken</h1>
                 <p className='text-3xl mt-2 text-gray-700 dark:text-gray-300 tracking-tighter'>The Solana Wallet Adaptor that let you what you want to do with your SOL</p>
+
             </div>
         </div>
     )
